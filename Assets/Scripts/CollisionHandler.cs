578af23441ebf41eee943c4a9b5aee259a2c4049
Dorotea <35565194+Dorole@users.noreply.github.com>
2022-05-14 20:38:05 +0200

    Added explosion effect on collision.

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,11 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace ArgonAssault
 {
     public class CollisionHandler : MonoBehaviour
     {
-        
+        [SerializeField] float _delayBeforeRestart = 1f;
+
+        void OnTriggerEnter(Collider other)
+        {
+            StartCrashSequence();
+        }
+
+        void StartCrashSequence()
+        {
+            gameObject.GetComponent<PlayerMovement>().enabled = false;
+            gameObject.GetComponent<PlayerShoot>().enabled = false;
+
+            StartCoroutine(CO_RestartLevel());
+        }
+
+        IEnumerator CO_RestartLevel()
+        {
+            int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+            yield return new WaitForSeconds(_delayBeforeRestart);
+            SceneManager.LoadScene(currentSceneIndex);
+        }
+
     }
 }
